50d890b08660a48d06527643eb1255a866a66e11
Lucas Smith <lucas@lucasesmith.com>
2022-05-01 20:41:07 -0400

    Add variable support.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import express from "express";
+import bodyParser from "body-parser";
 import { exec } from "child_process";
 const app = express()
 const port = 3000
@@ -7,11 +8,15 @@ const port = 3000
 const data = JSON.parse(fs.readFileSync("config.json"));
 console.log(data);
 
+app.use(bodyParser.json())
+
 for (const action in data.actions) {
   app.post(`/${data.key}/${action}`, (req, res) => {
     const actionToExec = data.actions[action];
     res.send(`Executing: ${actionToExec}`);
-    exec(actionToExec);
+    console.log(req.body);
+    const parsedActionToExec = replaceWithVals(actionToExec, req.body);
+    exec(parsedActionToExec);
   })
 }
 
@@ -26,3 +31,12 @@ if(data.startDelay){
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const replaceWithVals = (string, vals) => {
+  for(const val in vals){
+    const configStr = `%${val}`;
+    const passedVal = vals[val];
+    string = string.replaceAll(configStr, passedVal)
+  }
+  return string;
+}
